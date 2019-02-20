5f0b42729d550d96baf8e99f55cd2b1dfb3cf939
Thomas Louis <thomas.louis@misys.com>
2019-02-20 14:54:33 +0100

    first commit

---
+++ latexdockercmd.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "usage: $0 pdflatex -output-directory data/presentation/ beamer_pres.tex"
+IMAGE=latex-docker/base:ubuntu
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
