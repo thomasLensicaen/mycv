5061d9509bc0c4a2eaaa42fd1398b62a14dd4687
Thomas Louis <thomas.louis@misys.com>
2019-08-07 15:20:51 +0200

    add english cv

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "usage: $0 pdflatex -output-directory data/presentation/ beamer_pres.tex"
+echo "usage: $0 pdflatex -output-directory data/hyperparameter_optimization_detect.pdf hyperparameter_optimization_detect.tex"
 IMAGE=latex-docker/base:ubuntu
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" bash -c "cd data && $@"
